5c75ec97edf5b7e3caca536c3947981252aaea66
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2020-06-15 15:25:53 +0100

    changed button back to div - not fixed for mobile

--- script.js
+++ script.js
@@ -44,11 +44,11 @@ function searchRecipe(e) {
         recipeOutput.innerHTML = recipe
           .map(
             recipe => `
-          <button class="recipe">
+          <div class="recipe">
             <div class="recipe-info" data-recipeID="${recipe._id}">
               <p>${recipe.name}</p>
             </div>
-          </button>
+          </div>
         `
           )
           .join('');
